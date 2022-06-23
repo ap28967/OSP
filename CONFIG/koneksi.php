f625313b52483cc472ce80926a22f275fb2b4745
Mukhit Tobroni <Mukhit.Tobroni@daihatsu.astra.co.id>
2022-06-23 17:24:03 +0700

    kamis, 23-6-2022

--- CONFIG/koneksi.php
+++ CONFIG/koneksi.php
@@ -14,7 +14,7 @@ $dbname_osp = "osp_db";
 $link_osp = mysqli_connect($dbhost_osp,$dbuser_osp,$dbpass_osp,$dbname_osp);
 if (!$link_osp) {
     // die("<script>alert('Gagal tersambung dengan database.')</script>");
-    header("location: errors-404.html");
+    header("location: ../CONFIG/404.html");
 }
 
 ?>
